15cae708406e0520f2dbb9bb5c13a09d6d9d5dea
Ashraqat <ashrakatreda5@gmail.com>
2023-05-21 15:19:32 +0300

    first push of the project

---
+++ Exam_App/Exam student.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace sql_project
+{
+    partial class Exam_student
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Exam_student));
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(78, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(20, 118);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(158, 33);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.Visible = false;
+            this.radioButton1.Click += new System.EventHandler(this.radioButton1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.ForeColor = System.Drawing.Color.Chocolate;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(475, 321);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(77, 107);
+            this.button2.TabIndex = 3;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.ForeColor = System.Drawing.Color.Chocolate;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(574, 321);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 105);
+            this.button1.TabIndex = 4;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1204, 426);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Questions";
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(637, 224);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(158, 33);
+            this.radioButton4.TabIndex = 12;
+            this.radioButton4.Text = "radioButton4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.Visible = false;
+            this.radioButton4.Click += new System.EventHandler(this.radioButton4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1003, 20);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 29);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "10";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(985, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 29);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "/";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(954, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 29);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "1";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(20, 224);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(158, 33);
+            this.radioButton3.TabIndex = 6;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.Visible = false;
+            this.radioButton3.Click += new System.EventHandler(this.radioButton3_Click);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(637, 118);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(158, 33);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.Visible = false;
+            this.radioButton2.Click += new System.EventHandler(this.radioButton2_Click);
+            // 
+            // Exam_student
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1228, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Exam_student";
+            this.Text = "Exam_student";
+            this.Load += new System.EventHandler(this.Exam_student_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton radioButton4;
+    }
+}
---
+++ Exam_App/Exam student.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sql_project
+{
+    public partial class Exam_student : Form
+    {
+        ExamSYSEntities ent = new ExamSYSEntities();
+        int? exId;
+        List<exam_Questions_choices_Result> exQuestions = new List<exam_Questions_choices_Result>();
+        int questionIndex;
+        List<RadioButton> radios = new List<RadioButton>();
+        int radioIndex = 0;
+        int QuestionNum = 1;
+        List<Stu_Answer> std_answers= new List<Stu_Answer>();
+        public Exam_student()
+        {
+            InitializeComponent();
+            radios.Add(radioButton1);
+            radios.Add(radioButton2);
+            radios.Add(radioButton3);
+            radios.Add(radioButton4);
+
+        }
+
+        private void Exam_student_Load(object sender, EventArgs e)
+        {
+            Course crs = new Course();
+            crs = (from c in ent.Courses where c.Crs_Name == student_courses.crs_name select c).FirstOrDefault();
+            exId = ent.ShowExam(crs.Crs_Id).FirstOrDefault();
+            exQuestions = ent.exam_Questions_choices(exId).ToList();
+            questionIndex = 0;
+            label2.Text = QuestionNum.ToString();
+            foreach (var q in exQuestions)
+            {
+                bool included = false;
+                foreach(Stu_Answer ans in std_answers)
+                {
+                    if (ans.Q_Id == q.Q_Id)
+                    {
+                        included= true;
+                    }
+                }
+                if (included == false) 
+                {
+                    Stu_Answer answer = new Stu_Answer();
+                    answer.Ex_Id = (int)exId;
+                    answer.Q_Id = q.Q_Id;
+                    answer.St_Id = Form1.id;
+                    std_answers.Add(answer);
+                }
+            }
+            ChangeQuestion();
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            radioButton3.Checked = false;
+            radioButton4.Checked = false;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (QuestionNum != 10)
+            {
+                button1.Enabled = true;
+                QuestionNum++;
+                label2.Text = QuestionNum.ToString();
+                questionIndex++;
+                HideQuestionInfo();
+                ChangeQuestion();
+            }
+            else
+            {
+                button1.Enabled = false;
+                button2.Enabled = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (questionIndex > 0)
+            {
+                radioIndex = 0;
+                questionIndex--;
+                for (int i = 0; ; i++)
+                {
+                    if (exQuestions[questionIndex - i].Q_Id != exQuestions[questionIndex].Q_Id)
+                    {
+
+                        questionIndex -= i - 1;
+                        break;
+                    }
+                }
+                questionIndex--;
+                int counter = 0;
+                while (true)
+                {
+                    if (questionIndex - counter >= 0)
+                    {
+                        if (exQuestions[questionIndex - counter].Q_Id != exQuestions[questionIndex].Q_Id)
+                        {
+
+                            questionIndex -= counter - 1;
+                            break;
+                        }
+                        counter++;
+                    }
+                    else
+                    {
+                        questionIndex = 0;
+                        break;
+                    }
+                }
+            };
+            HideQuestionInfo();
+            ChangeQuestion();
+
+        }
+        public void ChangeQuestion()
+        {
+            radioIndex = 0;
+            label1.Text = exQuestions[questionIndex].Q_Title;
+            label1.Visible = true;
+            for (int i = 0; (questionIndex + i) <= exQuestions.Count - 1; i++)
+            {
+                if (exQuestions[questionIndex + i].Q_Id == exQuestions[questionIndex].Q_Id)
+                {
+
+                    radios[radioIndex].Text = exQuestions[questionIndex + i].choice;
+                    radios[radioIndex].Visible = true;
+                    radioIndex++;
+                }
+                else
+                {
+                    questionIndex += i - 1;
+                    break;
+                }
+
+            }
+            foreach(var ans in std_answers)
+            {
+                if (ans.Q_Id == exQuestions[questionIndex].Q_Id) 
+                {
+                    bool flag = false;
+                    foreach(RadioButton radio in radios)
+                    {
+                        if (radio.Text == ans.St_Answer)
+                        {
+                            radio.Checked = true;
+                            flag = true;
+                        }
+                    }
+                    if (flag == false)
+                    {
+                        radioButton1.Checked = false;
+                        radioButton2.Checked = false;
+                        radioButton3.Checked = false;
+                        radioButton4.Checked = false;
+                    }
+                }
+
+            }
+
+
+        }
+        public void HideQuestionInfo()
+        {
+            label1.Visible = false;
+            radioButton1.Visible = false;
+            radioButton2.Visible = false;
+            radioButton3.Visible = false;
+            radioButton4.Visible = false;
+        }
+        public void checkQuestion(string answer)
+        {
+            foreach (Stu_Answer ans in std_answers)
+            {
+                if (ans.Q_Id == exQuestions[questionIndex].Q_Id)
+                {
+                    ans.St_Answer=answer;
+                }
+
+            }
+        }
+        private void radioButton1_Click(object sender, EventArgs e)
+        {
+            checkQuestion(radioButton1.Text);
+        }
+
+        private void radioButton2_Click(object sender, EventArgs e)
+        {
+            checkQuestion(radioButton2.Text);
+        }
+
+        private void radioButton3_Click(object sender, EventArgs e)
+        {
+            checkQuestion(radioButton3.Text);
+        }
+
+        private void radioButton4_Click(object sender, EventArgs e)
+        {
+            checkQuestion(radioButton4.Text);
+        }
+    }
+}
